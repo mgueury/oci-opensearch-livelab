1e0fb35d58546ca6ef673c6ac5e2c97aea245a7a
Oracle Public Cloud User <opc@dev2.sub08301607040.mgsharedvcn.oraclevcn.com>
2022-10-27 14:19:08 +0000

    various fixes

---
+++ sync.sh
@@ -0,0 +1,5 @@
+cp -r ../oci-opensearch-devops/sample_files .
+cp -r ../oci-opensearch-devops/tika_parser .
+cp ../oci-opensearch-devops/oic/*.par oic
+cp ../oci-opensearch-devops/visual_builder/open*.zip visual_builder
+
